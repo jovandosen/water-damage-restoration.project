9e0dbe64fa56b169373cfd56e81621d4206c66a6
Jovan Dosen <43503548+dosenj@users.noreply.github.com>
2020-07-05 17:43:34 +0200

    Contact form moved to separate file

---
+++ wp-content/themes/water-damage-restoration/contact.php
@@ -0,0 +1,21 @@
+<div id="contact-container">
+    <div id="contact-form-container">
+        <form>
+            <div id="name-container">
+                <input type="text" name="name" id="name" placeholder="* Name" autocomplete="off">
+            </div>
+            <div id="email-container">
+                <input type="text" name="email" id="email" placeholder="* Email" autocomplete="off">    
+            </div>
+            <div id="phone-number-container">
+                <input type="text" name="phoneNumber" id="phone-number" placeholder="* Phone Number" autocomplete="off">
+            </div>
+            <div id="short-description-container">
+                <input type="text" name="shortDescription" id="short-description" placeholder="* Short Description" autocomplete="off">
+            </div>
+            <div id="contact-button-container">
+                <button type="button" id="contact-button">24/7 Emergency Services</button>
+            </div>
+        </form>
+    </div>
+</div>
