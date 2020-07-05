7de7621577d1db6988114af3eb2ddea0e65f2cc9
Jovan Dosen <43503548+dosenj@users.noreply.github.com>
2020-07-05 13:28:58 +0200

    Carousel selectors added

--- wp-content/themes/water-damage-restoration/assets/js/carousel.js
+++ wp-content/themes/water-damage-restoration/assets/js/carousel.js
@@ -1,17 +1,36 @@
 var current = timer = totalImages = 0;
+var selectorsDisplayed = false;
 
 function homepageCarousel()
 {
     var basePath = themeData.templateUrl;
     var images = ['forest.jpg', 'rocks.jpg', 'water.jpg'];
     var img = document.getElementById("carousel-img");
+    var imageSelectorsBox = document.getElementById("image-selectors-box");
     totalImages = images.length;
     if(current === totalImages){
         current = 0;
     }
     img.src = basePath + '/assets/images/' + images[current];
     current++;
+    if(!selectorsDisplayed){
+        selectorsDisplayed = true;
+        for(var i = 0; i < totalImages; i++){
+            var div = document.createElement("div");
+            div.setAttribute("class", "image-selector");
+            div.setAttribute("id", i);
+            div.addEventListener("click", changeImage);
+            imageSelectorsBox.appendChild(div);
+        }
+    }
     timer = setTimeout(homepageCarousel, 3000);
 }
 
 homepageCarousel();
+
+function changeImage()
+{
+    clearTimeout(timer);
+    current = parseInt(this.id);
+    homepageCarousel();
+}
--- wp-content/themes/water-damage-restoration/front-page.php
+++ wp-content/themes/water-damage-restoration/front-page.php
@@ -1,5 +1,8 @@
 <?php get_header(); ?>
     <div id="carousel-container">
         <img src="" alt="" width="100%" height="400px" id="carousel-img">
+        <div id="image-selectors-box">
+            <!-- Image selectors -->
+        </div>
     </div>
 <?php get_footer(); ?>        
