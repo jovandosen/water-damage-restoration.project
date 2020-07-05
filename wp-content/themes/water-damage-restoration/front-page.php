bf97c943bafb11534c445edb3125d2c243547323
Jovan Dosen <43503548+dosenj@users.noreply.github.com>
2020-07-05 15:36:24 +0200

    Carousel logic moved to separate file

--- wp-content/themes/water-damage-restoration/front-page.php
+++ wp-content/themes/water-damage-restoration/front-page.php
@@ -1,22 +1,3 @@
 <?php get_header(); ?>
-    <div id="carousel-container">
-        <img src="" alt="" width="100%" height="400px" id="carousel-img">
-        <div id="carousel-details">
-            <div id="title-one">
-                <h3 id="title-one-content"></h3>
-            </div>
-            <div id="title-two">
-                <h1 id="title-two-content"></h1>
-            </div>
-            <div id="description">
-                <p id="description-content"></p>
-            </div>
-            <div id="read-more-btn">
-                <button type="button" id="read-more-btn-content"></button>
-            </div>
-        </div>
-        <div id="image-selectors-box">
-            <!-- Image selectors -->
-        </div>
-    </div>
+    <?php get_template_part('carousel'); ?>
 <?php get_footer(); ?>        
