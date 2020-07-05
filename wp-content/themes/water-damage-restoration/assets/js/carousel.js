2d347b1c560393df9987cf498c8f8140ad7f9794
Jovan Dosen <43503548+dosenj@users.noreply.github.com>
2020-07-05 13:43:45 +0200

    Carousel selectors updated

--- wp-content/themes/water-damage-restoration/assets/js/carousel.js
+++ wp-content/themes/water-damage-restoration/assets/js/carousel.js
@@ -7,8 +7,12 @@ function homepageCarousel()
     var images = ['forest.jpg', 'rocks.jpg', 'water.jpg'];
     var img = document.getElementById("carousel-img");
     var imageSelectorsBox = document.getElementById("image-selectors-box");
+    var activeItem;
+    var inActiveItem;
     totalImages = images.length;
     if(current === totalImages){
+        inActiveItem = document.getElementById(current - 1);
+        inActiveItem.removeAttribute("style");
         current = 0;
     }
     img.src = basePath + '/assets/images/' + images[current];
@@ -23,6 +27,12 @@ function homepageCarousel()
             imageSelectorsBox.appendChild(div);
         }
     }
+    activeItem = document.getElementById(current - 1);
+    if(current > 1){
+        inActiveItem = document.getElementById(current - 2);
+        inActiveItem.removeAttribute("style");
+    }
+    activeItem.setAttribute("style", "background-color: red");
     timer = setTimeout(homepageCarousel, 3000);
 }
 
@@ -31,6 +41,10 @@ homepageCarousel();
 function changeImage()
 {
     clearTimeout(timer);
+    var allImages = document.getElementsByClassName("image-selector");
+    for(var i = 0; i < allImages.length; i++){
+        allImages[i].removeAttribute("style");
+    }
     current = parseInt(this.id);
     homepageCarousel();
 }
