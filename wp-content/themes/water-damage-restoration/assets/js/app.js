cb7d8f029118269318a9ca8dbc7693361763975b
Jovan Dosen <43503548+dosenj@users.noreply.github.com>
2020-07-05 11:25:51 +0200

    Sticky Navigation added and Info box removed on scroll

--- wp-content/themes/water-damage-restoration/assets/js/app.js
+++ wp-content/themes/water-damage-restoration/assets/js/app.js
@@ -1,8 +1,18 @@
 (function ($, window, document) {
     'use strict';
     $(document).ready(function () { 
+
         $("#toggle-nav-links").on("click", function(){
             $("#links-container").toggle('slow');
         });
+
+        $(window).scroll(function(){
+            if($(this).scrollTop() > 0){
+                $("#top-info").fadeOut();
+            } else {
+                $("#top-info").fadeIn();
+            }
+        });
+        
     });
 }(jQuery, window, document));
