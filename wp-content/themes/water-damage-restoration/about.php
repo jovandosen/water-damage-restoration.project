8517e2587afd4ba1531e30c95e02bfb2d30830d5
Jovan Dosen <43503548+dosenj@users.noreply.github.com>
2020-07-07 14:16:08 +0200

    About Us added and About Us Mobile Responsive design added

---
+++ wp-content/themes/water-damage-restoration/about.php
@@ -0,0 +1,49 @@
+<div id="about-us-box">
+    <div id="about-us-title-box">
+        <h1>About Us</h1>
+    </div>
+    <div id="about-us-data">
+        <div id="professionals-box">
+            <div id="professionals-image-box">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/forest.jpg" alt="Forest">
+            </div>
+            <div id="professionals-details">
+                <div id="professionals-details-title">
+                    <h3>Professionals</h3>
+                </div>
+                <div id="professionals-details-description">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua.</p>
+                </div>
+            </div>
+        </div>
+        <div id="trusted-box">
+            <div id="trusted-image-box">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/rocks.jpg" alt="Rocks">
+            </div>
+            <div id="trusted-details">
+                <div id="trusted-details-title">
+                    <h3>Trusted</h3>
+                </div>
+                <div id="trusted-details-description">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua.</p>
+                </div>
+            </div>
+        </div>
+        <div id="our-equipment-box">
+            <div id="our-equipment-image-box">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/water.jpg" alt="Water">
+            </div>
+            <div id="our-equipment-details">
+                <div id="our-equipment-details-title">
+                    <h3>Our Equipment</h3>
+                </div>
+                <div id="our-equipment-details-description">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
--- wp-content/themes/water-damage-restoration/front-page.php
+++ wp-content/themes/water-damage-restoration/front-page.php
@@ -2,4 +2,5 @@
     <?php get_template_part('carousel'); ?>
     <?php get_template_part('contact'); ?>
     <?php get_template_part('services'); ?>
+    <?php get_template_part('about'); ?>
 <?php get_footer(); ?>        
