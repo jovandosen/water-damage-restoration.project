898ca47f9c24b18b33c46b4cbe80bdffac105beb
Jovan Dosen <43503548+dosenj@users.noreply.github.com>
2020-07-12 22:57:37 +0200

    Update Contact Details form added to admin area

--- wp-content/themes/water-damage-restoration/assets/js/edit-contact-admin.js
+++ wp-content/themes/water-damage-restoration/assets/js/edit-contact-admin.js
@@ -22,5 +22,33 @@
 
         });
 
+        $(".contact-table-rows").on("click", function(){
+
+            $("#con-name").val('');
+            $("#con-email").val('');
+            $("#con-phone").val('');
+            $("#con-description").val('');
+
+            var nameData = $(this).children().eq(0).text();
+            var emailData = $(this).children().eq(1).text();
+            var phoneData = $(this).children().eq(2).text();
+            var descriptionData = $(this).children().eq(3).text();
+
+            $("#con-name").val(nameData);
+            $("#con-email").val(emailData);
+            $("#con-phone").val(phoneData);
+            $("#con-description").val(descriptionData);
+
+        });
+
+        $("#clear-form-btn").on("click", function(){
+
+            $("#con-name").val('');
+            $("#con-email").val('');
+            $("#con-phone").val('');
+            $("#con-description").val('');
+            
+        });
+
     });
 }(jQuery, window, document));
--- wp-content/themes/water-damage-restoration/show-contact-data.php
+++ wp-content/themes/water-damage-restoration/show-contact-data.php
@@ -28,7 +28,7 @@
             </thead>
             <tbody>
                 <?php while($row = mysqli_fetch_object($details)): ?>
-                    <tr class="contact-table-rows">
+                    <tr class="contact-table-rows" title="Click on table row to update contact details.">
                         <td><?php echo $row->name; ?></td>
                         <td><?php echo $row->email; ?></td>
                         <td><?php echo $row->phone; ?></td>
@@ -45,4 +45,31 @@
     <?php else: ?>
         <h3>No Contact Details Found.</h3>    
     <?php endif; ?>
+</div>
+<div id="update-contact-details">
+    <?php if($details->num_rows > 0): ?>
+        <div id="update-contact-details-form-box">
+            <div id="help-information">
+                <h3>Click on table row to update contact details</h3>
+            </div>
+            <form action="#">
+                <div id="update-contact-details-name-box">
+                    <input type="text" name="conName" id="con-name" autocomplete="off" placeholder="Name...">
+                </div>
+                <div id="update-contact-details-email-box">
+                    <input type="text" name="conEmail" id="con-email" autocomplete="off" placeholder="Email...">
+                </div>
+                <div id="update-contact-details-phone-box">
+                    <input type="text" name="conPhone" id="con-phone" autocomplete="off" placeholder="Phone...">
+                </div>
+                <div id="update-contact-details-description-box">
+                    <textarea name="conDescription" id="con-description" autocomplete="off" placeholder="Description..." rows="5"></textarea>
+                </div>
+                <div id="update-contact-details-button-box">
+                    <button type="button" id="update-contact-btn">UPDATE</button>
+                    <button type="button" id="clear-form-btn">CLEAR FORM</button>
+                </div>
+            </form>
+        </div>
+    <?php endif; ?>    
 </div>
